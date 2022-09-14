a58e990894b9662f1015721415c5a23e515eb337
Omar Jasseh <jassehomar99@gmail.comm>
2022-09-14 06:50:03 +0000

    assignment 3 - Interactive Rating Component

---
+++ Assignments/03-interactive-rating-component/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+  <>
+    <main>
+      <h1>Interactive Rating Component</h1>
+      {/* Your Card Component Here */}
+    </main>
+
+    <footer>
+      <div className="attribution">
+        <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+        </p>
+      </div>
+      <p className="copyright">&copy; JCC - Part 2 of the Junior Developer Program - 2022. All rights reserved.</p>
+    </footer>
+  </>
+  );
+}
+
+export default App;
