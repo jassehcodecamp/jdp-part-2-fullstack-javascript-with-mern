92d75f323dd51277e13cf957ec9642ae72c93a22
Omar Jasseh <jassehomar99@gmail.comm>
2022-09-23 16:13:53 +0000

    assignment 4 - Interactive Card Details

---
+++ Assignments/04-interactive-card-details/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      0000 0000 0000 0000 Jane Appleseed 00/00 000 Cardholder Name e.g. Jane
+      Appleseed Card Number e.g. 1234 5678 9123 0000 Exp. Date (MM/YY) MM YY CVC
+      e.g. 123 Confirm
+      {/* <!-- Completed state start --> */}
+      Thank you! We've added your card details Continue
+    </div>
+  )
+}
+
+export default App
