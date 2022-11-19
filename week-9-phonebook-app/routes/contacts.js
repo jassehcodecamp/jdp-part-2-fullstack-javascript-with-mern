4e54e0a3ce01ea51fced36bd42b59e30c33e0d73
Omar Jasseh <jassehomar99@gmail.comm>
2022-11-19 14:37:25 +0000

    implement CRUD (create, read, update, and delete) for Phonebook App

--- week-9-phonebook-app/routes/contacts.js
+++ week-9-phonebook-app/routes/contacts.js
@@ -1,14 +1,18 @@
 var express = require("express")
 var router = express.Router()
+const { v4: uuid } = require("uuid")
+const { route } = require("../app")
 
 const contacts = [
   {
+    id: uuid(),
     name: "Omar Jasseh",
     phone: "3100948",
     email: "omar@jassehcodecamp.com",
     address: "Bakoteh, Sanchaba",
   },
   {
+    id: uuid(),
     name: "Buba Conteh",
     phone: "3010451",
     email: "buba@jassehcodecamp.com",
@@ -23,24 +27,60 @@ router.get("/", function (req, res) {
   })
 })
 
+/* GET contact */
+router.get("/:id/edit", function (req, res) {
+  const id = req.params.id
+
+  const contact = contacts.find((contact) => contact.id === id)
+
+  res.render("contacts/edit", {
+    contact,
+  })
+})
+
 /* GET create contact. */
 router.get("/create", function (req, res) {
   res.render("contacts/create")
 })
 
-/* POST save contact. */
+/* POST: save contact. */
 router.post("/", function (req, res) {
-  // const name = req.body.name
-  // const email = req.body.email
-  // const phone = req.body.phone
-  // const address = req.body.address
-
   const { name, email, phone, address } = req.body
-  contacts.push({ name, email, phone, address })
+  contacts.push({ id: uuid(), name, email, phone, address })
 
   // console.log("contacts", contacts)
 
   // add flash message
+  res.redirect("/contacts")
+})
+
+/* Update: save contact */
+router.patch("/:id", function (req, res) {
+  const id = req.params.id
+
+  const formData = req.body
+
+  const contact = contacts.find((contact) => contact.id === id)
+  const contactIndex = contacts.indexOf(contact)
+
+  contact.name = formData.name
+  contact.phone = formData.phone
+  contact.email = formData.email
+  contact.address = formData.address
+
+  contacts[contactIndex] = contact
+
+  res.redirect("/contacts")
+})
+
+/* Delete: delete contact */
+router.delete("/:id", function (req, res) {
+  const contactIndex = contacts.findIndex(
+    (contact) => contact.id === req.params.id
+  )
+
+  contacts.splice(contactIndex, 1)
+
   res.redirect("/contacts")
 })
 
